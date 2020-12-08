c9bc95dfd5de22c42b9ffeef1c139f4e49c9ce04
Dmitriy Komovskiy <40181740+DK-SPB@users.noreply.github.com>
2020-12-08 12:28:31 +0300

    Initial commit

---
+++ links-backend/src/main/kotlin/server.kt
@@ -0,0 +1,29 @@
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.jetty.Jetty
+import kotlinx.html.*
+
+fun HTML.index() {
+    head {
+        title("Hello from Ktor!")
+    }
+    body {
+        div {
+            +"Hello from Ktor"
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Jetty, port = 8080, host = "127.0.0.1") {
+        routing {
+            get("/") {
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+        }
+    }.start(wait = true)
+}
